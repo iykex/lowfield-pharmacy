5b27132ff9e79468c3b87b582fefbe32dc939bbe
mr•vybes <aib.quame@gmail.com>
2026-08-05 04:18:37 +0000

    feat: add official pharmacy info, NHS URLs, and Pharmadoctor location links

--- lib/config/service-routing.ts
+++ lib/config/service-routing.ts
@@ -1,11 +1,14 @@
 import type { TenantSlug } from "@/lib/types/tenant";
 
 const PHARMADOCTOR_QUICK_LINKS: Record<TenantSlug, string> = {
-  belvedere: "https://www.phdr.co.uk/2483",
-  kidbrooke: "https://www.phdr.co.uk/500",
-  lowfield: "https://www.phdr.co.uk/6430",
+  belvedere:
+    "https://pharmadoctor.co.uk/patient/locations/london/dartford/BELVEDERE-PHARMACY--MECKAY-LIMITED--DA175QQ-2483",
+  kidbrooke:
+    "https://pharmadoctor.co.uk/patient/locations/london/london-se/Kidbrooke-Pharmacy-SE38AR-500",
+  lowfield:
+    "https://pharmadoctor.co.uk/patient/locations/london/dartford/Lowfield-Pharmacy-DA11HP-6430",
 };
 
 export function getApprovedPrivateBookingUrl(slug: TenantSlug): string {
-  return PHARMADOCTOR_QUICK_LINKS[slug];
+  return PHARMADOCTOR_QUICK_LINKS[slug] ?? "https://www.phdr.co.uk";
 }
---
+++ lib/constants/pharmacy-info.ts
@@ -0,0 +1,20 @@
+export const PHARMACY_INFO = {
+  name: "Lowfield Pharmacy",
+  slug: "lowfield",
+  postcode: "DA1 1HP",
+  city: "Dartford",
+  region: "London/Dartford",
+  pharmadoctorUrl:
+    "https://pharmadoctor.co.uk/patient/locations/london/dartford/Lowfield-Pharmacy-DA11HP-6430",
+  shortPharmadoctorUrl: "https://www.phdr.co.uk/6430",
+  nhsContactUrl:
+    "https://www.nhs.uk/services/pharmacy/lowfield-pharmacy/FLW40/contact-details-and-opening-times",
+  nhsServicesUrl:
+    "https://www.nhs.uk/services/pharmacy/lowfield-pharmacy/FLW40/services",
+  company: {
+    name: "MECKAY LIMITED",
+    registrationNumber: "06454698",
+    registeredIn: "UK",
+    address: "OAKHURST, ST PAULS WOOD HILL, BR5 2SR",
+  },
+} as const;
